d35cbb3942d4c513208592329d180e68d6ad26ae
ImeldaGtz <igutierrez22@ucol.mx>
2026-06-26 12:33:02 -0600

    Fin del episodio 6: Terreno simple

--- scene.py
+++ scene.py
@@ -3,6 +3,7 @@ from globals import *
 from sprite import Entity
 from player import Player
 from texture_data import solo_texture_data, atlas_texture_data
+from opensimplex import OpenSimplex
 
 class Scene:
     def __init__(self, app) -> None:
@@ -22,6 +23,8 @@ class Scene:
         Entity([self.sprites, self.blocks], pygame.Surface((TILESIZE*10, TILESIZE)) , position= (400,550))
 
         self.player = Player([self.sprites], self.solo_textures['player_static'], (SCREENWIDTH//2, SCREENHEIGHT//2), parameters={'block_group': self.blocks})
+
+        self.gen_world()
 
     def update(self):
         self.sprites.update()
@@ -50,4 +53,21 @@ class Scene:
         return textures
     
     def gen_world(self):
-        pass
+        noise_gen = OpenSimplex(seed=32500000)
+
+        heightmap = []
+        for y in range(20):
+            noise_value = noise_gen.noise2( y * 0.05, 0)
+            height = int( (noise_value +1) * 3 + 5 )
+            heightmap.append(height)
+
+        for x in range(len(heightmap)):
+            for y in range(heightmap[x]):
+                offset = 5-y + 5
+                texture = self.atlas_textures['dirt']
+                if y == heightmap[x] - 1:
+                    texture = self.atlas_textures['grass']
+
+                if y < heightmap[x] - 5:
+                    texture = self.atlas_textures['stone']
+                Entity( [self.sprites, self.blocks], texture, (x*TILESIZE, offset*TILESIZE) )
